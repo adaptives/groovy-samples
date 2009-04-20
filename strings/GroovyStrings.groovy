4dcc87b0b3077de8cdc17b3f1c6bcab75fdb2550
Parag Shah <adaptives@gmail.com>
2009-04-20 23:37:43 +0530

    added code to explain different quotes which can surround strings in Groovy

---
+++ strings/GroovyStrings.groovy
@@ -0,0 +1,18 @@
+/**
+ * This file shows how to use Strings in Groovy
+ */
+
+package strings
+
+//Groovy strings can be single quoted
+def str1 = 'hello world'
+println str1
+
+//Groovy Strings can be double quoted
+def str2 = "Hello World"
+
+//Groovy Strings can also be triple quoted. These strings can contain single
+//and double quotes and can be multiline
+def str3 = """This is a multiline Hello World and
+Namaste"""
+println str3
