36050ea1152ed23e57205210f13a7fb4942b37d8
Parag Shah <adaptives@gmail.com>
2009-05-01 20:13:58 +0530

    Added more stuff to GroovyLists.groovy

--- basic_examples/GroovyLists.groovy
+++ basic_examples/GroovyLists.groovy
@@ -45,9 +45,41 @@ println 'The list we just created is of type ' + aList.class.getName()
 
 //What if I want to create a LinkedList?
 def lList1 = new LinkedList()
-def lList2 = [] as LinkedList
+//Along with creating a LinkedList we are also instantiating the list
+def lList2 = ['bat'] as LinkedList
 assert lList1.class == LinkedList.class
 assert lList2.class == LinkedList.class
 
 //Let's push the limits of the 'as' operator
 println lList1 as String
+
+//We can append elements to the list using the << operator
+aList << 'Cat' << new Date()
+println aList
+
+//We can also use push and pop to add/remove elements from the list
+aList.push('Bat')
+println aList
+aList.pop()
+println aList
+
+//Groovy truth with lists
+println 'Groovy truth for aList is ' + (aList ? true : false)
+println 'Groovy truth for lList1 is ' + (lList1 ? true : false)
+
+//Get an element of the list using the [] operator
+aList << 'Elephant' << 'Rhino'
+println 'aList is ' + aList
+println 'The 2nd element in aList is ' + aList[1]
+println 'The 5th element in aList is ' + aList[5]
+println 'The second last element in aList is ' + aList[-2]
+println 'The sublist of elements 2..3 is ' + aList[1..2]
+println 'The sublist of the last two elements is ' + aList[-2..-1]
+
+//Sorting lists is a simple API call
+def unsortedList = [1,3,2,6,4]
+println 'unsorted list ' + unsortedList
+println 'sorted list ' + unsortedList.sort()
+
+//The sort method of list also tales a closure. We will look at that in the 
+//closures example - GroovyClosures.groovy
